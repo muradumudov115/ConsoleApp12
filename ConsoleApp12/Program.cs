3fb79e9ed40f4628e4bb51a9a4136d68dc8e12a3
muradumudov115 <murad@DESKTOP-TQKOADC>
2021-10-22 15:49:28 +0400

    Add project files.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student obj1 = new Student();
+            obj1.name = "murad";
+            obj1.surname = "umudov";
+            obj1.age = 18;
+
+            
+            Student obj2 = new Student();
+            obj2.name = "sahil";
+            obj2.surname = "mamedov";
+            obj2.age = 15;
+            Console.WriteLine(obj1.Fullname());
+            Console.WriteLine();
+            Console.WriteLine(obj2.Fullname());
+
+
+        }
+    }
+    class Student
+    {
+        public string name;
+        public string surname; 
+        public int age;
+        public string Fullname()
+        {
+            return $"{name} {surname}";
+        }
+    }
+}
